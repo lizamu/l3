2859023a71a90edbe79f26c478bb058dbf6dd059
LittelHell13 <hottelmich@gmail.com>
2019-02-27 09:25:25 +0700

    v1

--- l3/l3/Program.cs
+++ l3/l3/Program.cs
@@ -10,15 +10,33 @@ namespace l3
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter A: ");
-                int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Выбирите пункт меню \n   1)\n    2)\n    3)");
+            string q = Console.ReadLine();
+            int a = 0; int b = 0;
+            start:
+            switch (q)
+            {
+                case "1":
+                        Console.WriteLine("\n\n");
+                        Console.Write("Enter A: ");
+                        a = int.Parse(Console.ReadLine());
+                    goto start;
+                    break;
+                case "2":
+                    Console.WriteLine("\n\n");
+                    Console.Write("Enter B: ");
+                    b = int.Parse(Console.ReadLine());
+                    break;
+                case "3":
+                    Console.WriteLine("\n\n");
+                    Console.WriteLine("A-B = {0}", a + b);
+                    break;
 
-                Console.Write("Enter B: ");
-                int b = int.Parse(Console.ReadLine());
+            }
+            
+            
 
-
-                int c = a + b;
-                Console.WriteLine("A+B = {0}", c);
+               /*
 
                 int d = a - b;
                 Console.WriteLine("A-B = {0}", d);
@@ -30,7 +48,7 @@ namespace l3
                 Console.WriteLine("A/B = {0}", h);
             Console.WriteLine("A/B = {0}", h);
             Console.WriteLine("A/B = {0}", h);
-            Console.WriteLine("A/B = {0}", h);
+            Console.WriteLine("A/B = {0}", h);*/
 
         }
     }
